b5853a96b5e626c0f59f4b820c088a73ede84649
Joao Gouveia <j.lucas.gouveia@gmail.com>
2020-03-29 14:08:10 +0100

    is link block checking, but must be improved

--- src/parser.h
+++ src/parser.h
@@ -6,9 +6,10 @@ void fromJson(char* path)
 	char buffer[5000];
 	FILE* json;
 	json = fopen(path, "r");
-	int count = 0;
+	int blockCounter = 0; // must finish == 0 otherwise json is invalid
 
 	int linePosition = 0;
+	int isLinkBlock = 0;
 
 	/*reading file*/
 	while(fgets(buffer, sizeof(buffer), json) != NULL){
@@ -16,11 +17,28 @@ void fromJson(char* path)
 		/*reading line*/
 		while(buffer[linePosition] != '\0'){
 			if(buffer[linePosition] != '\t' && buffer[linePosition] != ' ' && buffer[linePosition] != '\n'){
-				printf("%c", buffer[linePosition]);
+				//block counter control
+				if(buffer[linePosition] == '{' || buffer[linePosition] == '['){
+					blockCounter++;
+				}else if(buffer[linePosition] == '}' || buffer[linePosition] == ']'){
+					blockCounter--;
+				}
+
+				if(buffer[linePosition] == '"' &&
+				 buffer[linePosition + 1] == 'l' &&
+				  buffer[linePosition + 2] == 'i' &&
+				   buffer[linePosition + 3] == 'n' &&
+				    buffer[linePosition + 4] == 'k' &&
+				     buffer[linePosition + 5] == 's' &&
+				      buffer[linePosition + 6] == '"'){
+					isLinkBlock = 1;
+				}
+
 			}
 			linePosition++;
 		}
 	}
+	printf("isLinkBlock = %i\n", isLinkBlock);
 }
 
 #endif
